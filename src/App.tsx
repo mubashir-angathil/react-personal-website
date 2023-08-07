0ecfc6c21cec289702492f4ecf331d4f13154325
Mubashir Angathil <mubashirangathil5142@gmail.com>
2023-08-08 02:59:32 +0530

    Basic About layer design created

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,56 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useRef } from 'react'
-import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax'
 import './App.css';
+import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax'
 import profile from '../src/assets/profile-pic.png'
 interface PageProps {
   offset: number
   gradient?: string
   onClick: () => void
 }
-
 const Page = ({ offset, onClick }: PageProps) => (
   <React.Fragment>
-    {/* <ParallaxLayer offset={offset} speed={0.3} onClick={onClick}>
+    {/* <ParallaxLayer offset={offset} speed={0.3}  onClick={onClick}>
       <div className={'slopeBegin'} />
     </ParallaxLayer> */}
     {
       offset === 0
         ?
-        <>
-          <ParallaxLayer className='banner' offset={offset} speed={1} factor={0.5}>
-            <div className='title-wrapper'>
-              <h1 >
-                <span>Hello!</span><br />
-                I'm
-                <span>Mubashir</span><br />
-              </h1>
-              <h4> MERN Stack Developer </h4>
-            </div>
-            <div>
-              <img src={profile} />
-            </div>
-          </ParallaxLayer>
-        </>
-
+        <ParallaxLayer className='banner' offset={offset} factor={0.8} speed={0.2}>
+          <div className='title-wrapper'>
+            <h1 >
+              <span>Hello!</span><br />
+              I'm
+              <span>Mubashir</span><br />
+            </h1>
+            <h4> MERN Stack Developer </h4>
+          </div>
+          <div>
+            <img src={profile} alt='DP' />
+          </div>
+        </ParallaxLayer>
         : null
     }
     {
       offset === 1
         ?
-        <>
-          <ParallaxLayer className='about-section' offset={offset} speed={0.3}>
+        <ParallaxLayer offset={offset} speed={1}>
+          <div className='about-section'>
+            <div>
+              <img src={profile} alt='DP' />
+            </div>
             <div>
               <span className='name'>About Me</span>
-              <p>
-                I am flexible, reliable and possess excellent time keeping skills.
-                I am an enthusiastic, self-motivated, reliable, responsible and hard working person.
-                I am a mature team worker and adaptable to all challenging situations.
-                I am able to work well both in a team environment as well as using own initiative.
-              </p>
+              <div>
+                <p>name     :<span>"Mubashir Ali Angathil"</span><span>,</span></p>
+                <p>dob      :<span>"June 23, 2001"</span><span>,</span></p>
+                <p>domain   :<span>"MERN Stack"</span><span>,</span></p>
+                <p>Location :<span>"MERN Stack"</span><span>,</span></p>
+                <p>email    :<span>"mail.mubashirangathil@gmail.com"</span><span></span></p>
+              </div>
             </div>
-          </ParallaxLayer>
-        </>
-
+          </div>
+        </ParallaxLayer>
         : null
     }
   </React.Fragment>
@@ -69,20 +68,23 @@ export default function App() {
   React.useEffect(() => {
     setOffset(0)
   }, [])
-
   return (
-    <Parallax className='container' ref={parallax} pages={3} horizontal={false}>
-      <ParallaxLayer sticky={{start:0.9,end:3}}>
+    <Parallax className='container' ref={parallax} pages={2} horizontal={false}>
+      <ParallaxLayer sticky={{ start: 0.9 }}>
         <div className='navbar'>
           <ul>
             {
-              ['Home', 'About', 'Contact', 'Resume'].map((item, index) =>
-                <li key={item}>
+              ['Home', 'About', 'Resume'].map((item, index) =>
+                <li
+                  key={item}
+                  className={index === offset ? 'active-item' : ''}
+                >
                   <a
-                    onClick={() => index!==3 ? scroll(index): null} 
-                    className={index === offset ? 'active-border' : ''}
-                    download={item === 'Resume' ? true : false}
-                    href={item === 'Resume' ? '#home' : undefined}
+                    className={index === offset ? 'active-item' : ''}
+                    onClick={() => index !== 2 ? scroll(index) : null}
+                    // download={item === 'Resume' ? tr/ : false}
+                    target='_blank'
+                    href={item === 'Resume' ? 'https://docs.google.com/document/d/1S0BlG2AB-5yKlCjWbLQab8AmVpTu8XMmyKgN8nVEcMQ/edit?usp=sharing' : undefined} rel="noreferrer"
                   >{item}</a></li>
               )
             }
@@ -90,12 +92,12 @@ export default function App() {
         </div>
 
       </ParallaxLayer>
-      {/* <ParallaxLayer offset={offset} speed={1} >
-        <div className='slopeEnd tomato' />
+      {/* <ParallaxLayer speed={1} >
+        <div className='slopeEnd dark' />
       </ParallaxLayer> */}
-      <Page offset={0} onClick={() => scroll(1)} />
-      <Page offset={1} onClick={() => scroll(2)} />
-      <Page offset={2} onClick={() => scroll(0)} />
+      <Page offset={0} onClick={() => scroll(0)} />
+      <Page offset={1} onClick={() => scroll(1)} />
+      {/* <Page offset={2} onClick={() => scroll(2)} /> */}
     </Parallax >
   )
 }
